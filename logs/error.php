c1b2db51cf2e04f7d06ec6b0dc1c73f43cd1ea45
moldanova <moldanova93@gmail.com>
2015-02-23 14:44:53 +1000

    Refresh Joomla

---
+++ logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-07-30 05:51:52 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2013-07-30	05:51:52	INFO	127.0.0.1	Joomla FAILURE: 	Имя пользователя и пароль не совпадают или у вас еще нет учетной записи на сайте
+2013-10-23	14:10:14	INFO	127.0.0.1	Joomla FAILURE: 	Имя пользователя и пароль не совпадают или у вас еще нет учетной записи на сайте
+2015-02-23	03:59:00	INFO	127.0.0.1	Joomla FAILURE: 	Имя пользователя и пароль не совпадают или у вас еще нет учетной записи на сайте
